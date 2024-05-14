20da63c65c4ad3cf9b4ada3a62fd15b0157cf09c
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-05-14 05:03:43 +0300

    add auth with kinde

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,15 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 type Props = {};
 
-export default function Navbar({}: Props) {
-  const user = undefined;
-  const isAdmin = false;
+export default async function Navbar({}: Props) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
