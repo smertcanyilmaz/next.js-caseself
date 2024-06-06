206316c17f6c660462727705105cc3df92ff4794
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-06-06 06:05:23 +0300

    add react-query

---
+++ src/components/Provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import { ReactNode } from "react";
+
+const client = new QueryClient();
+
+export default function Provider({ children }: { children: ReactNode }) {
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
